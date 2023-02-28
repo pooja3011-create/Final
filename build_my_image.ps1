59f4f6c13ced3e44df21153774412e2a8bf5a2c3
Pooja Patel <pp30111991@gmail.com>
2023-02-28 13:38:40 +0100

    Update build_my_image.ps1

--- build_my_image.ps1
+++ build_my_image.ps1
@@ -1,20 +1,24 @@
-#$HOMEDRIVE = "C:\"
-#$HOMEPATH = "Users\" + $env:username
+# Set the Docker image tag and computer name
+$DOCKER_IMAGE_TAG = "dockerhelperimage"
+$COMPUTER_NAME = $env:COMPUTERNAME
 
-#Docker image name
-$DockerHelperImageTag = "dockerhelperimage"
-#name of a computer, where Docker is installed
-$ComputerName = $env:computername
-Function Get-BuildImage {
-    
+# Define a function to build the Docker image
+function Get-BuildImage {
+    # Show the list of Docker images
     docker images
 
+    # Open the Dockerfile in Visual Studio Code
     code .\Dockerfile
 
-    docker build -t $DockerHelperImageTag .
+    # Build the Docker image with the specified tag
+    docker build -t $DOCKER_IMAGE_TAG .
 
+    # Show the updated list of Docker images
     docker images
 }
+
+# Call the function to build the Docker image
 Get-BuildImage
-#Run-DockerContainer 
+
+# Call the script to run the Docker container
 .\run_my_container.ps1
